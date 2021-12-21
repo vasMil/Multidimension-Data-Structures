4cf5f89ef05ee9458ef97f8967f9217cf569d7c3
manolisgiannakis <manosgian2000@gmail.com>
2021-12-21 20:27:31 +0200

    Augmented Interval Tree
    Building the augmented interval tree with the maxes in each node.
    Searching for overlaps between the query interval and the intervals that consist the tree.

--- IntervalTree/interval.py
+++ IntervalTree/interval.py
@@ -5,26 +5,27 @@ class Node:
         self.interval = interval
         self.left_child = None
         self.right_child = None
-        self.Max = interval[1]
+        self.Max = None
 
-    # def has_child(self):
-    #     if self.left_child or self.right_child:
-    #         return True
-    #     return False
+    def has_child(self):
+        if self.left_child or self.right_child:
+            return True
+        return False
 
 
 class IntervalTree:
     def __init__(self, root):
         self.root = root
 
-    # Builds the tree and matches the correct Max in each node--->
+    # Builds the tree and matches the correct Max in each node-->
 
     def addition(self, new_node):
         node = self.root
 
         while node is not None:
-            if new_node.interval[1] > node.Max:
-                node.Max = new_node.interval[1]
+
+            # if new_node.interval[1] > node.Max:
+            #     node.interval[2] = new_node.interval[1]
 
             if new_node.interval[0] <= node.interval[0]:
                 if node.left_child is None:
@@ -45,11 +46,13 @@ class IntervalTree:
 
         if self.overlapping(c_node.interval, query_interval):
             overlapping_nodes.append(c_node.interval)
+            print(c_node.interval)
 
         if (c_node.left_child is not None) and (c_node.left_child.Max >= query_interval[0]):
             self.search_for_overlaps(c_node.left_child, query_interval)
 
-        self.search_for_overlaps(c_node.right.child, query_interval)
+        if (c_node.right_child is not None) and (c_node.right_child.Max >= query_interval[0]):
+            self.search_for_overlaps(c_node.right_child, query_interval)
 
     def overlapping(self, interval1, interval2):
         if interval1[0] <= interval2[1] and interval2[0] <= interval1[1]:
@@ -57,7 +60,7 @@ class IntervalTree:
 
         return False
 
-    # in-order traversal
+    # in-order traversal-->
 
     def print_tree(self, node):
         if node is None:
@@ -70,6 +73,27 @@ class IntervalTree:
         if node.right_child is not None:
             self.print_tree(node.right_child)
 
+    # finds the max for each node-->
+
+    def maxes(self, root_node):
+        if (root_node.Max is None) and (root_node.has_child()):
+            max_array = []
+            if root_node.left_child:
+                self.maxes(root_node.left_child)
+                max_array.append(root_node.left_child.Max)
+            if root_node.right_child:
+                self.maxes(root_node.right_child)
+                max_array.append(root_node.right_child.Max)
+            max_array.append(root_node.interval[1])
+            root_node.Max = max(max_array)
+            root_node.interval.append(root_node.Max)
+            return
+
+        else:
+            root_node.Max = root_node.interval[1]
+            root_node.interval.append(root_node.Max)
+            return
+
 
 t_root = Node([5, 10])
 It = IntervalTree(t_root)
@@ -79,4 +103,7 @@ It.addition(Node([8, 16]))
 It.addition(Node([14, 20]))
 It.addition(Node([18, 21]))
 It.addition(Node([2, 8]))
-It.print_tree(t_root)
+# It.addition(Node([8, 21]))
+It.maxes(t_root)
+# It.print_tree(t_root)
+It.search_for_overlaps(t_root, [10, 15])
