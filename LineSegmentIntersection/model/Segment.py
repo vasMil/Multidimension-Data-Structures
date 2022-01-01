efd26aa97fb626c3bcae115f9478636a996aac45
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-02 00:54:22 +0200

    Changes Segment constructor so pt1 is the highest point of the two

--- LineSegmentIntersection/model/Segment.py
+++ LineSegmentIntersection/model/Segment.py
@@ -1,7 +1,11 @@
 class Segment:
     def __init__(self, pt1, pt2):
-        self.pt1 = pt1
-        self.pt2 = pt2
+        if pt1 > pt2:
+            self.pt1 = pt1
+            self.pt2 = pt2
+        else:
+            self.pt1 = pt2
+            self.pt2 = pt1
 
     def __gt__(self, other):
         if(self.getHigherPriorityPoint().x > other.getHigherPriorityPoint().x):
@@ -22,16 +26,10 @@ class Segment:
         return self.toString()
 
     def getLowerPriorityPoint(self):
-        if (self.pt1 < self.pt2):
-            return self.pt1
-        else:
             return self.pt2
 
     def getHigherPriorityPoint(self):
-        if (self.pt1 > self.pt2):
-            return self.pt1
-        else:
-            return self.pt2
+        return self.pt1
 
     def toString(self):
         return '{' + self.pt1.toString() + ', ' + self.pt2.toString() + '}'
